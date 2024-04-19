3d597a8595048e3a03975e88cde7851feeadc2fc
Александр Дудукало <alexandr.dudukalo@gmail.com>
2024-04-20 00:36:47 +0300

    fix(HomeView.vue): исправит страницу

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,5 @@
 <script setup>
 import FormIp from "../components/FormIp.vue";
-
-watch(myRef, (newValue, oldValue) => {
-  console.log(`Значение ref изменилось с ${oldValue} на ${newValue}`);
-  lastChange.value = `изменилось с ${oldValue} на ${newValue}`;
-});
 </script>
 
 <template>
